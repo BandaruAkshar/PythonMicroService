4ab6a59cf19a0797e418c5cc9a9dcc8f1e21a0af
Bandaru Akshar <129412748+BandaruAkshar@users.noreply.github.com>
2024-07-25 06:57:56 +0000

    adding fastapi

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wiki_logic
+from mylib.logic import phrase as wiki_phrase
 
-result = wiki()
-print(result)
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Wikipedia API. call/search_wiki"}
+
+@app.get("/search/{value}")
+async def search(value:str):
+    """Page to Search Wikipedia"""
+    result = search_wiki(value)
+    return {"result":result}
+
+@app.get("/wiki/{name}")
+async def wiki(name:str):
+    """Retrieve Wikipedia Page"""
+    result = wiki_logic(value)
+    return {"result"}
+
+@app.get("/phrase/{name}")
+async def phrase(name:str):
+    result = wiki_phrase(name)
+    return {"result":result}
+if __name__ == "__main__":
+    uvicorn.run(app,port=8080,host='0.0.0.0')
+# result = wiki()
+# print(result)
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,5 +1,5 @@
 import wikipedia
-
+from textblob import TextBlob
 
 def wiki(name="War Goddess", length=1):
     """This is a Wikipedia fetcher"""
@@ -10,3 +10,10 @@ def search_wiki(name):
     """Search wikipedia for names"""
     results = wikipedia.search(name)
     return results
+
+def phrase(name):
+    """Returns Phrases from Wikipedia"""
+    page = wiki(name)
+    blob = TextBlob(page)
+    phrases = blob.noun_phrases
+    return phrases
